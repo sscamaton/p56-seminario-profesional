70cbd44ea0349f9cafa3d5c11bb54976d708f22e
sscamaton <sscamaton@gmail.com>
2020-10-02 15:19:52 -0500

    Ejemplo de Javascript basico

---
+++ code/javascript/02_string.js
@@ -0,0 +1,19 @@
+var nombre = 'sulay' 
+var apellido = 'camatom'
+
+var nombre = 'sulay', apellido = 'camatom'
+
+var nombreMayusculas = nombre.toUpperCase()
+var apellidoMayusculas = apellido.toUpperCase()
+
+
+console.log( nombreMayusculas+' '+apellidoMayusculas)
+console.log('${nombre} ${apellidoMayusculas}')
+
+var primeraLetraNombre = nombre.charAt(0)
+var longitudNombre = nombre.length
+var subcadenaNombre = nombre.substr(1,3)
+
+console.log( primeraLetraNombre)
+console.log( longitudNombre)
+console.log( subcadenaNombre)
